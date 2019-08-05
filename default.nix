f40192fcfd1f2ecd92c1a61fc971e87593d049cf
Nathan Braswell <miloignis@gmail.com>
2019-08-05 05:40:02 +0000

    My Nix env...

---
+++ default.nix
@@ -0,0 +1,20 @@
+{ pkgs ? import <nixpkgs> {} }:
+
+(pkgs.buildFHSUserEnv {
+  name = "rust-vulkan-env";
+  targetPkgs = pkgs: (with pkgs;
+    [
+      pkg-config
+      binutils-unwrapped
+      glxinfo
+      wayland
+      libGL
+      vulkan-loader
+      xorg.libX11
+      xorg.libX11.dev
+      xorg.libXcursor
+      xorg.libXrandr
+      xorg.libxcb
+    ]);
+  runScript = "bash -c \"export PKG_CONFIG_PATH='${pkgs.xorg.libX11.dev}/lib/pkgconfig:${pkgs.xorg.xorgproto}/share/pkgconfig' && bash\"";
+}).env
