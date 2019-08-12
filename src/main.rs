256f75b2c7698b1b54aa6d5dd7f802755014da03
Nathan Braswell <miloignis@gmail.com>
2019-08-12 06:24:46 +0000

    Port over triangle example

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,141 @@
+use wgpu::winit::*;
+
 fn main() {
     println!("Hello, world!");
+
+    let mut events_loop = EventsLoop::new();
+    let window = Window::new(&events_loop).unwrap();
+    let size = window.get_inner_size().unwrap().to_physical(window.get_hidpi_factor());
+    let instance = wgpu::Instance::new();
+    let surface = instance.create_surface(&window);
+    let adapter = instance.get_adapter(&wgpu::AdapterDescriptor {
+        power_preference: wgpu::PowerPreference::LowPower,
+    });
+    let mut device = adapter.request_device(&wgpu::DeviceDescriptor {
+        extensions: wgpu::Extensions {
+            anisotropic_filtering: false,
+        },
+        limits: wgpu::Limits::default(),
+    });
+
+    fn compile_shadercode(code: &str, stage: glsl_to_spirv::ShaderType) -> Vec<u32> {
+        use std::io::Read;
+
+        let mut spv_bytes = Vec::new();
+        glsl_to_spirv::compile(&code, stage).unwrap().read_to_end(&mut spv_bytes).unwrap();
+        let mut spv_words = Vec::new();
+        for bytes4 in spv_bytes.chunks(4) {
+            spv_words.push(u32::from_le_bytes([bytes4[0], bytes4[1], bytes4[2], bytes4[3]]));
+        }
+        spv_words
+    }
+
+    let vs_words = compile_shadercode(r#"
+    #version 450
+    out gl_PerVertex {
+        vec4 gl_Position;
+    };
+    const vec2 positions[3] = vec2[3](
+        vec2( 0.0, -0.5),
+        vec2( 0.5,  0.5),
+        vec2(-0.5,  0.5)
+    );
+    void main() {
+        gl_Position = vec4(positions[gl_VertexIndex], 0.0, 1.0);
+    }"#, glsl_to_spirv::ShaderType::Vertex);
+    let vs_module = device.create_shader_module(&vs_words);
+    let fs_words = compile_shadercode(r#"
+    #version 450
+    layout(location = 0) out vec4 outColor;
+    void main() {
+        outColor = vec4(1.0, 0.0, 0.0, 1.0);
+    }"#, glsl_to_spirv::ShaderType::Fragment);
+    let fs_module = device.create_shader_module(&fs_words);
+
+    let bind_group_layout = device.create_bind_group_layout(&wgpu::BindGroupLayoutDescriptor { bindings: &[]});
+    let bind_group = device.create_bind_group(&wgpu::BindGroupDescriptor {
+        layout: &bind_group_layout,
+        bindings: &[],
+    });
+    let pipeline_layout = device.create_pipeline_layout(&wgpu::PipelineLayoutDescriptor {
+        bind_group_layouts: &[&bind_group_layout],
+    });
+    let render_pipeline = device.create_render_pipeline(&wgpu::RenderPipelineDescriptor {
+        layout: &pipeline_layout,
+        vertex_stage: wgpu::PipelineStageDescriptor {
+            module: &vs_module,
+            entry_point: "main",
+        },
+        fragment_stage: Some(wgpu::PipelineStageDescriptor {
+            module: &fs_module,
+            entry_point: "main",
+        }),
+        rasterization_state: wgpu::RasterizationStateDescriptor {
+            front_face: wgpu::FrontFace::Ccw,
+            cull_mode: wgpu::CullMode::None,
+            depth_bias: 0,
+            depth_bias_slope_scale: 0.0,
+            depth_bias_clamp: 0.0,
+        },
+        primitive_topology: wgpu::PrimitiveTopology::TriangleList,
+        color_states: &[wgpu::ColorStateDescriptor {
+            format:      wgpu::TextureFormat::Bgra8UnormSrgb,
+            color_blend: wgpu::BlendDescriptor::REPLACE,
+            alpha_blend: wgpu::BlendDescriptor::REPLACE,
+            write_mask:  wgpu::ColorWrite::ALL,
+        }],
+        depth_stencil_state: None,
+        index_format: wgpu::IndexFormat::Uint16,
+        vertex_buffers: &[],
+        sample_count: 1,
+    });
+
+    let mut swap_chain = device.create_swap_chain(
+        &surface,
+        &wgpu::SwapChainDescriptor {
+            usage: wgpu::TextureUsage::OUTPUT_ATTACHMENT,
+            format: wgpu::TextureFormat::Bgra8UnormSrgb,
+            width: size.width.round() as u32,
+            height: size.height.round() as u32,
+            present_mode: wgpu::PresentMode::Vsync,
+        },
+    );
+    let mut running = true;
+    while running {
+        events_loop.poll_events(|event| match event {
+            Event::WindowEvent { event, .. } => match event {
+                WindowEvent::KeyboardInput { input: KeyboardInput { virtual_keycode: Some(code),
+                                                                    state: ElementState::Pressed,
+                                                                    ..
+                                                                  },
+                                              ..
+                } => match code {
+                    VirtualKeyCode::Escape => running = false,
+                    _ => {},
+                },
+                WindowEvent::CloseRequested => running = false,
+                _ => {},
+            },
+            _ => {}
+        });
+
+        let frame = swap_chain.get_next_texture();
+        let mut encoder = device.create_command_encoder(&wgpu::CommandEncoderDescriptor { todo : 0 });
+        {
+            let mut rpass = encoder.begin_render_pass(&wgpu::RenderPassDescriptor {
+                color_attachments: &[wgpu::RenderPassColorAttachmentDescriptor {
+                    attachment: &frame.view,
+                    resolve_target: None,
+                    load_op: wgpu::LoadOp::Clear,
+                    store_op: wgpu::StoreOp::Store,
+                    clear_color: wgpu::Color::GREEN,
+                }],
+                depth_stencil_attachment: None,
+            });
+            rpass.set_pipeline(&render_pipeline);
+            rpass.set_bind_group(0, &bind_group, &[]);
+            rpass.draw(0..3, 0..1);
+        }
+        device.get_queue().submit(&[encoder.finish()]);
+    }
 }
