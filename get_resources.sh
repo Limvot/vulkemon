7ff4aef89025f041328faef390a9757f8d9c6879
Nathan Braswell <miloignis@gmail.com>
2019-08-12 23:21:45 +0000

    Resources script

---
+++ get_resources.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env bash
+
+FOLDER="resources"
+
+mkdir -p "$FOLDER"
+
+ensure_downloaded() {
+    url="$1"
+    file="$2"
+    if [ ! -f "$FOLDER/$file" ]
+    then
+        echo "curl \"$url\" --output \"$FOLDER/$file\""
+        curl "$url" --output "$FOLDER/$file"
+    fi
+}
+
+ensure_downloaded "https://www.spriters-resource.com/resources/sheets/18/19776.png" "littleroot.png"
